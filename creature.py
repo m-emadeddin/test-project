cf46af26bc72ccdc8d23c638ade7c1c71ec5f919
m-emadeddin <muhammed.emadeddin@gmail.com>
2025-04-20 00:09:26 +0200

    Created Abstract Creature Class

---
+++ creature.py
@@ -0,0 +1,9 @@
+class Createure:
+    def __init__(self, name, health, attack, defense):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+
+    def __str__(self):
+        return f"{self.name} (Health: {self.health}, Attack: {self.attack}, Defense: {self.defense})"
--- hello.py
+++
@@ -1,4 +0,0 @@
-print("Hello, World!")
-name = input("Please Enter your name: ")
-print(f"Hello, {name}")
-print("Please Exit the program by pressing BLA BLA BLA")
